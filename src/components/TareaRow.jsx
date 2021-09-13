8b73fab7ded98c994076626eb79a6f5829b2af50
JavierLoera <78636781+JavierLoera@users.noreply.github.com>
2021-09-13 15:57:34 -0500

    Update TareaRow.jsx

--- src/components/TareaRow.jsx
+++ src/components/TareaRow.jsx
@@ -1,23 +1,9 @@
 import React from "react";
-import { TableCell, TableRow } from "@material-ui/core";
+import { TableCell, TableRow, Button } from "@material-ui/core";
 import { Link } from "react-router-dom";
-import { makeStyles } from "@material-ui/core/styles";
 import DeleteIcon from "@material-ui/icons/Delete";
-import CreateIcon from "@material-ui/icons/Create";
 import IconButton from "@material-ui/core/IconButton";
-import Box from "@material-ui/core/Box";
-import { red, cyan } from "@material-ui/core/colors";
-
-const useStyles = makeStyles((theme) => ({
-	button: {
-		margin: theme.spacing(1),
-	},
-	root: {
-		"& > *": {
-			margin: theme.spacing(1),
-		},
-	},
-}));
+import { red } from "@material-ui/core/colors";
 
 export default function TareaRow({
 	id,
@@ -26,28 +12,24 @@ export default function TareaRow({
 	eliminarTarea,
 	modificarTarea,
 }) {
-	const classes = useStyles();
-
 	return (
 		<TableRow>
 			<TableCell>{tarea}</TableCell>
 			<TableCell align="center">{descripcion}</TableCell>
 			<TableCell>
-				<Box className={classes.root}>
-					<IconButton onClick={() => eliminarTarea(id)} aria-label="delete">
-						<DeleteIcon style={{ color: red[500] }} />
-					</IconButton>
-					<Link to="/Modificar">
-						<IconButton
-							onClick={() => modificarTarea(id)}
-							aria-label="delete"
-							disabled
-							color="primary"
-						>
-							<CreateIcon style={{ color: cyan[700] }} />
-						</IconButton>
-					</Link>
-				</Box>
+				<IconButton onClick={() => eliminarTarea(id)} aria-label="delete">
+					<DeleteIcon style={{ color: red[500] }} />
+				</IconButton>
+
+				<Link to="/Modificar" style={{ textDecoration: "none" }}>
+					<Button
+						variant="outlined"
+						color="primary"
+						onClick={() => modificarTarea(id)}
+					>
+						Modificar
+					</Button>
+				</Link>
 			</TableCell>
 		</TableRow>
 	);
